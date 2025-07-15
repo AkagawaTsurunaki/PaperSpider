c43c7bee037aecc4368a3ed9516428bcd93e4c4d
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-07-15 14:53:39 +0800

    fix: 不确定行数的情况下的rank解析问题

--- parser/detail_parser.py
+++ parser/detail_parser.py
@@ -15,37 +15,22 @@ def fetch_detail_page(journalid: str):
 
 def parse_detail(html_doc: str):
     tree = html.fromstring(html_doc)
-    # 中科院分区
-    cas2025_type = None
-    cas2025_type_elms = tree.xpath('//*[@id="yxyz_content"]/table[3]/tbody/tr[29]/td[2]/table/tr[2]/td[1]')
-    if len(cas2025_type_elms) == 1:
-        cas2025_type = cas2025_type_elms[0].text.strip()
-
-    cas2025_rank = None
-    cas2025_rank_elms = tree.xpath('//*[@id="yxyz_content"]/table[3]/tbody/tr[29]/td[2]/table/tr[2]/td[1]/span[2]')
-    if len(cas2025_rank_elms) == 1:
-        cas2025_rank = cas2025_rank_elms[0].text.strip()
-
-    cas2023_type = None
-    cas2023_type_elms = tree.xpath('//*[@id="yxyz_content"]/table[3]/tbody/tr[30]/td[2]/table/tr[2]/td[1]')
-    if len(cas2023_type_elms) == 1:
-        cas2023_type = cas2023_type_elms[0].text.strip()
-
-    cas2023_rank = None
-    cas2023_rank_elms = tree.xpath('//*[@id="yxyz_content"]/table[3]/tbody/tr[30]/td[2]/table/tr[2]/td[1]/span[2]')
-    if len(cas2023_rank_elms) == 1:
-        cas2023_rank = cas2023_rank_elms[0].text.strip()
-
-    cas2022_type = None
-    cas2022_type_elms = tree.xpath('//*[@id="yxyz_content"]/table[3]/tbody/tr[31]/td[2]/table/tr[2]/td[1]')
-    if len(cas2022_type_elms) == 1:
-        cas2022_type = cas2022_type_elms[0].text.strip()
-
-    cas2022_rank = None
-    cas2022_rank_elms = tree.xpath('//*[@id="yxyz_content"]/table[3]/tbody/tr[31]/td[2]/table/tr[2]/td[1]/span[2]')
-    if len(cas2022_rank_elms) == 1:
-        cas2022_rank = cas2022_rank_elms[0].text.strip()
-
+    cas_result = []
+    # [!WARNING] 这里可能需要修改，但是目前它能跑，主要问题在于详细页面可能会少几行或者多几行
+    for i in range(20, 50):
+        cas_type = tree.xpath(f'//*[@id="yxyz_content"]/table[3]/tbody/tr[{i}]/td[2]/table/tr[2]/td[1]')
+        if len(cas_type) == 1:
+            cas_type = cas_type[0].text
+            if cas_type is not None:
+                cas_result.append(cas_type.strip())
+        cas_rank = tree.xpath(f'//*[@id="yxyz_content"]/table[3]/tbody/tr[{i}]/td[2]/table/tr[2]/td[1]/span[2]')
+        if len(cas_rank) == 1:
+            cas_rank = cas_rank[0].text
+            if cas_rank is not None:
+                cas_result.append(cas_rank.strip())
+    cas2025_type, cas2025_rank = cas_result[0], cas_result[1]
+    cas2023_type, cas2023_rank = cas_result[2], cas_result[3]
+    cas2022_type, cas2022_rank = cas_result[4], cas_result[5]
     result = {}
 
     if cas2025_type is None and cas2025_rank is None:
